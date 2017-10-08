3b0366e8070de032d1937e2d4255a7a10adbd225
sfsdfs <fsdfds>
2017-10-08 18:00:18 -0400

    save

--- milesplit.py
+++ milesplit.py
@@ -3,6 +3,7 @@
 import urllib.request
 from bs4 import BeautifulSoup as soup
 CATEGORIES = ["","article","athlete","meet","team","video"]
+MILESPLIT = "http://milesplit.com"
 SEARCH_URL = "http://{}milesplit.com/search?q={}&category={}"
 class NotValid(Exception):
     pass
@@ -24,7 +25,10 @@ def search_for(item, category="",state=None):
     raw_results = page.find("div",{"class":"searchResults"}).find("ul").findAll("li")
     results = []
     for raw_result in raw_results:
-        results.append({raw_result.find("a").text.strip():{"link":raw_result.find("a")["href"]},"description":raw_result.find("div",{"class":"description"}).text.strip(),"type":raw_result.find("span",{"class":"type"}).text.strip()})
+        link = raw_result.find("a")
+        results.append({link.text.strip():{"link":MILESPLIT + link["href"]},"id":link["href"].split("/")[-1],"description":raw_result.find("div",{"class":"description"}).text.strip(),"type":raw_result.find("span",{"class":"type"}).text.strip()})
     return results
-results = search_for("x",category="athlete",state="nc")
-print(results)
+
+results = search_for("hough",category="",state="nc")
+for result in results:
+    print(result)
